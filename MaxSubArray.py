3c2c46d676cb3ac04bc09d3c29cd065de03ea51c
Nishant Thakur <nt.nishantt@gmail.com>
2018-01-15 23:21:25 -0600

    test
    lol

--- MaxSubArray.py
+++ MaxSubArray.py
@@ -1,28 +1,56 @@
-def findSum(nums, low, high):
-    if low == high:
-        return nums[low]
-    else:
-        n = high+low
-        mid = n//2
-        print("Index:",low, mid, high)
-        left_sum = findSum(nums, low, mid)
-        right_sum = findSum(nums, mid+1, high)
-        cross_sum = left_sum + right_sum
-
-        print(left_sum,cross_sum,right_sum)
-        return max(left_sum,cross_sum,right_sum)
-
-
+import sys
 class Solution:
+
     def maxSubArray(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        high = len(nums)-1
-        max_sum = findSum(nums, 0, high)
-        # print(max_sum)
+        if not nums:
+            return 0
+        n = len(nums)
+        low, high, max_sum  = self.findMaxSubArray(nums, 0, n-1)
+        # print(low, high, max_sum)
+
         return max_sum
+
+    def findMaxSubArray(self, nums, low, high):
+        if low == high:
+            return (low, high, nums[low])
+        else:
+            mid = (low + high) // 2;
+
+            leftl, lefth, left_sum = self.findMaxSubArray(nums, low, mid)
+            rightl, righth, right_sum = self.findMaxSubArray(nums, mid + 1, high)
+            crossl, crossh, cross_sum = self.findMaxCrossSubArray(nums, low, mid, high)
+
+            if left_sum >= right_sum and left_sum >= cross_sum:
+                return (leftl, lefth, left_sum)
+            if right_sum >= left_sum and right_sum >= cross_sum:
+                return (rightl, righth, right_sum)
+            else:
+                return (crossl, crossh, cross_sum)
+
+
+    def findMaxCrossSubArray(self, nums, low, mid, high):
+        left_sum = -sys.maxsize
+        sum = 0
+        for i in range(mid, -1, -1):
+            sum = sum + nums[i]
+            if sum > left_sum:
+                left_sum = sum
+                max_left = i
+
+        right_sum = -sys.maxsize
+        sum = 0
+        for j in range(mid+1, high+1):
+            sum = sum + nums[j]
+            if sum > right_sum:
+                right_sum = sum
+                max_right = j
+
+        return (max_left, max_right, left_sum + right_sum)
+
 
 
 test = Solution()
