e4f5dd6b10fe11335d1d19c03362b2a96a240d0f
Nishant Thakur <nt.nishantt@gmail.com>
2018-01-27 23:06:17 -0600

    solution to problem 653
    in Python

---
+++ 653.TwoSum4-forBST.py
@@ -0,0 +1,56 @@
+"""
+Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+Example 1:
+Input:
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 9
+
+Output: True
+Example 2:
+Input:
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 28
+
+Output: False
+"""
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def __init__(self):
+        self.dict = {}
+
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+
+        if root.val in self.dict:
+            return True
+
+        self.dict[k - root.val] = True
+
+        left = self.findTarget(root.left, k)
+        right = self.findTarget(root.right, k)
+
+        return left or right
+
