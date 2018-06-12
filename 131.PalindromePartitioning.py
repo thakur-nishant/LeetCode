63e2eeb117a51aed996e0e034848d7c88072a73c
Nishant Thakur <nt.nishantt@gmail.com>
2018-06-12 11:34:10 -0500

    longest Palindrome with logic: expand around center
    in python

---
+++ 131.PalindromePartitioning.py
@@ -0,0 +1,36 @@
+"""
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+Example:
+
+Input: "aab"
+Output:
+[
+  ["aa","b"],
+  ["a","a","b"]
+]
+"""
+
+
+class Solution:
+    def partition(self, s):
+        res = []
+        self.dfs(s, [], res)
+        return res
+
+    def dfs(self, s, path, res):
+        if not s:
+            res.append(path)
+            return
+        for i in range(1, len(s) + 1):
+            if self.isPal(s[:i]):
+                self.dfs(s[i:], path + [s[:i]], res)
+
+    def isPal(self, s):
+        return s == s[::-1]
+
+s = "aaabb"
+x = Solution().partition(s)
+print(x)
