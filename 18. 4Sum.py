597c7721577b035b5940a8206b62066bfa56616f
Nishant Thakur <nt.nishantt@gmail.com>
2019-04-21 23:59:10 -0500

    Solution to 18

---
+++ 18. 4Sum.py
@@ -0,0 +1,38 @@
+"""
+Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+Note:
+
+The solution set must not contain duplicate quadruplets.
+
+Example:
+
+Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+
+A solution set is:
+[
+  [-1,  0, 0, 1],
+  [-2, -1, 1, 2],
+  [-2,  0, 0, 2]
+]
+"""
+
+## Bruit force solution
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = set()
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                for k in range(j + 1, len(nums)):
+                    for l in range(k + 1, len(nums)):
+                        if nums[i] + nums[j] + nums[k] + nums[l] == target:
+                            result.add((nums[i], nums[j], nums[k], nums[l]))
+
+        return [list(x) for x in sorted(result)]
+
